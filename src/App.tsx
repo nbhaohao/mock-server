1f574abd68c4afc333f6167aaddf8c78483ce2f3
zhangzh <zhangzh@cnlemon.net>
2019-09-29 20:55:05 +0800

    init: 初始化项目

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import "./styles/reset.scss";
+import { Manage } from "@/pages/Manage";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/manage" />
+        </Route>
+        <Route path="/manage" exact>
+          <Manage />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Manage/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Manage: React.FC = () => {
+  return <div>Manage</div>;
+};
+
+export { Manage };
