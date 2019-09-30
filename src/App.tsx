7e653270f934b540a6bbb8656ad9d29ec4af6977
zhangzh <zhangzh@cnlemon.net>
2019-09-30 16:02:44 +0800

    feat: 完成基本的页面布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
-import "./styles/reset.scss";
-import { Manage } from "@/pages/Manage";
+import { Layout } from "@/components/Layout";
+import "./styles/index.scss";
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/manage" />
-        </Route>
-        <Route path="/manage" exact>
-          <Manage />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  return <Layout />;
 };
 
 export default App;
