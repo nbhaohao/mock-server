4b13e234df6ff908c5b744b810605e2a043c6dc5
nbhaohao <nbhaohao@163.com>
2019-11-18 21:37:03 +0800

    fix: 修复项目重复渲染的严重 bug

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,17 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import { Header } from "@/components/Header";
 import "./index.scss";
 import { Content } from "@/components/Content";
 
 const Layout: React.FC = () => {
   return (
-    <Router>
       <div className="layout-component">
         <Header />
         <Content />
       </div>
-    </Router>
   );
 };
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useMemo } from "react";
 import * as _modules from "./models";
 import { ModelType } from "@/store/models/type";
 
@@ -27,6 +27,7 @@ export interface EffectType {
 const modules: ModelsType = _modules;
 const GLOBAL_LOADING_ACTION = "CHANGE_LOADING";
 const GLOBAL_UPDATE_STATE = "UPDATE_STATE";
+let refactorEffectsTag: boolean = false;
 const initialState: InitialStateType = {
   loading: {}
 };
@@ -52,6 +53,9 @@ const reducer = (state: InitialStateType, action: ActionType) => {
 };
 
 const refactorModuleEffects = (dispatch: DispatchType) => {
+  if (refactorEffectsTag) {
+    return;
+  }
   for (const module in modules) {
     const moduleItem: ModelType = modules[module];
     initialState[moduleItem.namespace] = moduleItem.state;
@@ -92,60 +96,67 @@ const refactorModuleEffects = (dispatch: DispatchType) => {
       };
     }
   }
+  refactorEffectsTag = true;
 };
 
 const useStore = (): [InitialStateType, DispatchType, EffectType] => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const _dispatch = (action: ActionType) => {
-    if (action.type === GLOBAL_LOADING_ACTION) {
-      dispatch({
-        type: GLOBAL_LOADING_ACTION,
-        payload: action.payload
-      });
-      return;
-    }
-    const [namespace, actionName] = action.type.split("/");
-    const moduleItem = modules[namespace];
-    if (!moduleItem) {
-      return;
-    }
-    const reducers = moduleItem.reducers;
-    if (!reducers) {
-      return;
-    }
-    const reducerMethod = reducers[actionName];
-    if (!reducerMethod) {
-      return;
-    }
-    const newNamespaceState = reducerMethod(moduleItem.state, action.payload);
-    dispatch({
-      type: GLOBAL_UPDATE_STATE,
-      payload: {
-        [namespace]: newNamespaceState
+  const _dispatch = useMemo(
+    () => (action: ActionType) => {
+      if (action.type === GLOBAL_LOADING_ACTION) {
+        dispatch({
+          type: GLOBAL_LOADING_ACTION,
+          payload: action.payload
+        });
+        return;
       }
-    });
-  };
+      const [namespace, actionName] = action.type.split("/");
+      const moduleItem = modules[namespace];
+      if (!moduleItem) {
+        return;
+      }
+      const reducers = moduleItem.reducers;
+      if (!reducers) {
+        return;
+      }
+      const reducerMethod = reducers[actionName];
+      if (!reducerMethod) {
+        return;
+      }
+      const newNamespaceState = reducerMethod(moduleItem.state, action.payload);
+      dispatch({
+        type: GLOBAL_UPDATE_STATE,
+        payload: {
+          [namespace]: newNamespaceState
+        }
+      });
+    },
+    [dispatch]
+  );
   refactorModuleEffects(_dispatch);
-  const effect = (action: ActionType) => {
-    const [namespace, actionName] = action.type.split("/");
-    const moduleItem = modules[namespace];
-    if (!moduleItem) {
-      return;
-    }
-    const effects = moduleItem.effects;
-    if (!effects) {
-      return;
-    }
-    const effectMethod = effects[actionName];
-    if (!effectMethod) {
-      return;
-    }
-    return effectMethod(action.payload, {
-      state: moduleItem.state,
-      rootState: state,
-      dispatch: _dispatch
-    });
-  };
+  const effect = useMemo(
+    () => (action: ActionType) => {
+      const [namespace, actionName] = action.type.split("/");
+      const moduleItem = modules[namespace];
+      if (!moduleItem) {
+        return;
+      }
+      const effects = moduleItem.effects;
+      if (!effects) {
+        return;
+      }
+      const effectMethod = effects[actionName];
+      if (!effectMethod) {
+        return;
+      }
+      return effectMethod(action.payload, {
+        state: moduleItem.state,
+        rootState: state,
+        dispatch: _dispatch
+      });
+    },
+    [state, _dispatch]
+  );
   return [state, _dispatch, effect];
 };
 
