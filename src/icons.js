031ecb8f03a4cb4c98c61d1431f4f3a09e2ffca8
nbhaohao <nbhaohao@163.com>
2019-12-01 21:02:20 +0800

    feat: 优化 ant-design icon 大小的问题 (https://github.com/ant-design/ant-design/issues/12011)

---
+++ src/icons.js
@@ -0,0 +1,3 @@
+export { default as EditOutline } from "@ant-design/icons/lib/outline/EditOutline";
+export { default as TableOutline } from "@ant-design/icons/lib/outline/TableOutline";
+export { default as DeleteOutline } from "@ant-design/icons/lib/outline/DeleteOutline";
